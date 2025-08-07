acb9e21bc7f5285aa07b15c7f3cac464c16d2467
Lucas Gomes Santana <ls7128387@gmail.com>
2025-08-07 20:54:41 -0300

    Iniciando o Projeto

---
+++ front-end/script.js
@@ -0,0 +1,44 @@
+const video = document.getElementById('video')
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo)
+
+function startVideo() {
+  navigator.mediaDevices.getUserMedia({ video: {} })
+    .then(stream => {
+      video.srcObject = stream;
+      video.onloadedmetadata = () => {
+        video.play().catch(err => console.log("Error to play the video:", err));
+      };
+    })
+    .catch(err => console.error("Error on camera:", err));
+}
+
+video.addEventListener('playing', () => {  // Replacing 'play' for 'playing'
+  console.log("Vídeo pronto para reprodução!");
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+
+  setInterval(async () => {
+    try {
+      const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+        .withFaceLandmarks()
+        .withFaceExpressions();
+
+      const resizedDetections = faceapi.resizeResults(detections, displaySize);
+      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+      faceapi.draw.drawDetections(canvas, resizedDetections);
+      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+      faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+
+    } catch (err) {
+      console.error("Error on face detection:", err);  
+    }
+  }, 100);
+});
