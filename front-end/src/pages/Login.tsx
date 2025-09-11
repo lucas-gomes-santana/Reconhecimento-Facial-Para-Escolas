bb15153695a8a0e53578cd1643787de7d944aa3a
Lucas Gomes Santana <ls7128387@gmail.com>
2025-09-11 20:39:03 -0300

    Migrando gerenciador de pacotes para pnpm

--- front-end/src/pages/Login.tsx
+++ front-end/src/pages/Login.tsx
@@ -118,10 +118,10 @@ function Login() {
                             type="submit"
                             disabled={loading}
                             className="w-full py-3 px-4 text-white font-semibold rounded-lg 
-                                    bg-blue-600 hover:bg-blue-700 cursor-pointer focus:outline-none 
+                                    bg-blue-600 hover:bg-blue-700 focus:outline-none 
                                     focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all
                                     disabled:bg-gray-400 disabled:cursor-not-allowed shadow-md
-                                    hover:shadow-lg transform hover:scale-105"
+                                    hover:shadow-lg "
                         >
                             {loading ? (
                                 <div className="flex items-center justify-center">
