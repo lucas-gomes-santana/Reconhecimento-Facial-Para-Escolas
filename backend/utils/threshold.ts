0aa024cb7cee68f30301271e1ea1ac1ba568be0c
lucas-gomes-santana <lucasgomessantana259@gmail.com>
2026-08-31 10:01:59 -0300

    refactor: correção de tipagem no serviço de comparação de rostos no backend

--- backend/utils/threshold.ts
+++ backend/utils/threshold.ts
@@ -1,1 +1,6 @@
-export const threshold = 0.96; // Percentual mínimo de 96% de similaridade para sucesso na autenticação facial
+/* Esta constante define um percentual mínimo de 96% de similaridade para definir
+que um rosto escaneado no frontend é o mesmo de um rosto do banco de dados após todos os
+vetores faciais serem comparados. Se após todas as verificações nenhuma comparação bate esse
+percentual, o sistema entende que o rosto escaneado não existe no banco*/
+
+export const threshold = 0.96;
